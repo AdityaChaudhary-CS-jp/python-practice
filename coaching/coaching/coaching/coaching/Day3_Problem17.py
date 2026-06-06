373a772ae3f961f9d92ebae9d83996c69b3f6214
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-06 23:00:42 +0530

    Day 3 - salary increment program

---
+++ coaching/coaching/coaching/coaching/Day3_Problem17.py
@@ -0,0 +1,7 @@
+# Problem17
+a = input('Name = ')
+b = input('Father\'s name = ')
+c = int(input('Enter Your Salary = '))
+d = int(input('Enter Increment = '))
+e = c+d
+print("Congratulations,",a,"S/O",b,",your salary now is",e,"after increment of",d,',have a nice day')
