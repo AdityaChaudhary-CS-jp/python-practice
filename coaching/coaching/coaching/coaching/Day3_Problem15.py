6a879a7886ce0588997df9d46924e2be9868f806
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-06 22:59:04 +0530

    Day 3 - sum of last digits

---
+++ coaching/coaching/coaching/coaching/Day3_Problem15.py
@@ -0,0 +1,9 @@
+# Problem15
+a = int(input('Enter any no = '))
+b = int(input('Enter any no = '))
+C = int(input('Enter any no = '))
+d = a%10
+e = b%10
+f = C%10
+K = d+e+f
+print('Sum of last digits = ',K)
