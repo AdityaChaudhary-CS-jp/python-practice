0c29ea06ba8e6b7875f64741c763db0b2aafc423
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-08 22:42:07 +0530

    Day 5 - square cube and roots calculation

---
+++ coaching/coaching/coaching/coaching/Day5_Problem23.py
@@ -0,0 +1,10 @@
+# Problem23
+a = int(input('Input a number = '))
+b = a**2
+c = a**(1/2)
+d = a**3
+e = a**(1/3)
+print('The square of',a,'is',b)
+print('The square root of',a,'is',c)
+print('The cube of',a,'is',d)
+print('The cube root of',a,'is',e)
