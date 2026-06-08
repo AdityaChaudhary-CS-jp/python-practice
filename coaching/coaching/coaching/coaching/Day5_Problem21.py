e5349133f4fbf861d026518b95942ad1d9ab0a81
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-08 22:40:41 +0530

    Day 5 - seconds to hours minutes and seconds

---
+++ coaching/coaching/coaching/coaching/Day5_Problem21.py
@@ -0,0 +1,7 @@
+# Problem21
+a = int(input('Enter the number of seconds = '))
+b = a//3600
+c = a%3600
+d = c//60
+e = c%60
+print(a,'seconds is equal to',b,'hour(s),',d,'minute(s) and',e,'second(s).')
