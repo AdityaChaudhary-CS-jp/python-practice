e36157030a73e5f6e365c2bcb2f69be958339748
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-08 22:41:19 +0530

    Day 5 - currency notes breakdown program

---
+++ coaching/coaching/coaching/coaching/Day5_Problem22.py
@@ -0,0 +1,19 @@
+# Problem22
+a = int(input('Enter the amount = '))
+b = a//500
+c = a%500
+d = c//200
+e = c%200
+f = e//100
+g = e%100
+h = g//50
+i = g%50
+j = i//10
+k = i%10
+print(a,'is equal to')
+print(b,'500 rupees note(s),')
+print(d,'200 rupees note(s),')
+print(f,'100 rupees note(s),')
+print(h,'50 rupees note(s),')
+print(j,'10 rupees note(s) and')
+print(k,'rupee(s).')
