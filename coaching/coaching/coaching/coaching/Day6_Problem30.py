c3dc6c075ab5aaec413a2377e97e0470940a9921
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-09 21:04:42 +0530

    Day 6 - passing percentage criteria verification program

---
+++ coaching/coaching/coaching/coaching/Day6_Problem30.py
@@ -0,0 +1,8 @@
+# Problem30
+a = int(input("Enter 1st Subject Marks : "))
+b = int(input("Enter 2nd Subject Marks : "))
+c = int(input("Enter 3rd Subject Marks : "))
+k = (a+b+c)/300
+d = k*100
+e = 45
+print(d>=e)
