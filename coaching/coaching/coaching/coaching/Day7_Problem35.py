bf04d9282d82811b1800040f0eb2f64859272c4c
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-10 21:56:36 +0530

    Day 7 - bitwise operators

---
+++ coaching/coaching/coaching/coaching/Day7_Problem35.py
@@ -0,0 +1,26 @@
+# Problem35
+# Part-1
+a = 10
+b = 7
+c = a&b
+print(c)
+# Part-2
+a = 41
+b = 13
+c = a|b
+print(c)
+# Part-3
+a = 15
+b = 5 
+c = a^b
+print(c)
+# Part-4
+a = 20  
+b = 2
+c = a>>b
+print(c)
+# Part-5
+a = 10
+b = 3
+c = a<<b
+print(c)
