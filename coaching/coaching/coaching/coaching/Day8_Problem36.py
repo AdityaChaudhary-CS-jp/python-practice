d3e18a86eb24fcde0fe20177293ebcf02ebfc9e6
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-11 22:13:19 +0530

    Day 8 - shift operators multiply and divide

---
+++ coaching/coaching/coaching/coaching/Day8_Problem36.py
@@ -0,0 +1,8 @@
+# Problem36((i)a number multiply it by 4 by shift operator.  (ii)a number divide it by 2 by shift operator)
+a = int(input('Enter a no = '))
+k = a<<2
+print(k)
+
+a = int(input('Enter a no = '))
+k = a>>1
+print(k)
