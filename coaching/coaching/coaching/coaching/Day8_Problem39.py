fbb5573aa02d7c41cc6e2941100dd95faef5ad51
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-11 22:15:06 +0530

    Day 8 - if with or season classifier

---
+++ coaching/coaching/coaching/coaching/Day8_Problem39.py
@@ -0,0 +1,8 @@
+# Problem39
+a = int(input('Enter a month number : '))
+if a==4 or a==5 or a==6 :
+    print('Summer')
+if a==7 or a==8 :
+    print('Rainy')
+if a==11 or a==12 or a==1:
+    print('Winter')
