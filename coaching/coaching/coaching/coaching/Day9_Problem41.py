b47447a06387093b613396e52629a3505b6f8bf9
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-12 15:45:40 +0530

    Day 9 - calculator using if operator selection

---
+++ coaching/coaching/coaching/coaching/Day9_Problem41.py
@@ -0,0 +1,15 @@
+# Problem41
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = input("Enter an operator (+, -, *, /): ")
+if c == '+':
+    print(a + b)
+
+if c == '-':
+    print(a - b)
+
+if c == '*':
+    print(a * b)
+
+if c == '/':
+    print(a / b)
