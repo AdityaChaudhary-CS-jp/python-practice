a3a203c63deb5255336f81f9f8e1d75a76934900
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-16 22:03:24 +0530

    Day 13 - divisibility checker if elif else

---
+++ coaching/coaching/coaching/coaching/Day13_Problem55.py
@@ -0,0 +1,10 @@
+# Problem55
+a = int(input('Enter a Number = '))
+if a%3==0 :
+    print('Divisible by 3')
+elif a%5==0 :
+    print('Divisible by 5')
+elif a%11==0 :
+    print('Divisible by 11')
+else:
+    print('None')
