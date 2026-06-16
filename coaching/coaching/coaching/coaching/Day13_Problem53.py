c66edb55401a610640cfbd8c1fbad95caaf63c9e
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-16 22:02:02 +0530

    Day 13 - triangle type using if elif else

---
+++ coaching/coaching/coaching/coaching/Day13_Problem53.py
@@ -0,0 +1,10 @@
+# Problem53
+a = int(input('Enter 1st side : '))
+b = int(input('Enter 2nd side : '))
+c = int(input('Enter 3rd side : '))
+if a==b and a==c:
+    print('Equilateral Triangle')
+elif a==b or b==c or c==a :
+    print('Isosceles Triangle')
+else:
+    print('Scalene Triangle')
