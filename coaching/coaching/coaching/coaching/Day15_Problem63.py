9cfd28ebe551119e93bced8385e9c1a048d59d3b
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-18 21:33:10 +0530

    Day 15 - for loop introduction with range function

---
+++ coaching/coaching/coaching/coaching/Day15_Problem63.py
@@ -0,0 +1,10 @@
+# Problem63
+for k in range (5):
+      print('Ok', end =' ')
+      print('done')
+# AND
+for p in range(5,2,3):
+     print('Pen')
+#AND
+for m in range(2,2):
+      print('Stool')
