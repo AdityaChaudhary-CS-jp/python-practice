4847318179da9f66a30d078dbb6c5909fbf5541d
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-19 22:11:53 +0530

    Day 16 - for loop sequence step generation and ASCII character printing via chr()

---
+++ coaching/coaching/coaching/coaching/Day16_Problem65.py
@@ -0,0 +1,11 @@
+# Problem65
+n =int(input('Enter a number = '))
+for m in range(1,n+1,2):
+    print(m,end=' ')
+
+# AND
+
+k =int(input('Enter a number = '))
+p =int(input('Enter a number = '))
+for i in range(k,p+1):
+    print(chr(i),end =' ')
