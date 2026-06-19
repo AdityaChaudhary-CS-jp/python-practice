594586c5c9ce21c9caf3c5555948efb37e988e34
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-19 22:11:07 +0530

    Day 16 - multiple for loop range variations for ascending, descending, and stepped sequences

---
+++ coaching/coaching/coaching/coaching/Day16_Problem64.py
@@ -0,0 +1,22 @@
+# Problem64
+n =int(input('Enter How many time message to display = '))
+a = input('Enter the message = ')
+for k in range(n) :
+    print(a)
+
+# AND
+
+l =int(input('Enter a number = '))
+for p in range(1,l+1):
+    print(p, end='  ')
+
+# AND
+g = int(input('Enter a number = '))
+for e in range(g,-1,-1):
+    print(e,end=' ')
+
+# AND
+
+w = int(input('Enter a number = '))
+for i in range(0,w+1,2):
+    print(i,end = ' ')
