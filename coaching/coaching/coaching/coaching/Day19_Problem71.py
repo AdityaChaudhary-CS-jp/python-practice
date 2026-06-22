f8c8da9335d68b1bda519fea125af5efbeea672a
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-22 21:43:42 +0530

    Day 19 - sort three numbers ascending with equal values handled(Modified old question)

---
+++ coaching/coaching/coaching/coaching/Day19_Problem71.py
@@ -0,0 +1,20 @@
+# Problem61
+#Modified for some values same
+a = int(input('Enter 1st Number  '))
+b = int(input('Enter 2nd Number  '))
+c = int(input('Enter 3rd Number  '))
+if a<=b and a<=c :
+    if b<=c:
+        print('Ascending ', a,b,c)
+    else :
+        print('Ascending ', a,c,b)
+elif b<a and b<c :
+    if a < c :
+        print('Ascending ',b,a,c )
+    else:
+        print('Ascending ', b,c,a)
+else :
+    if b<a :
+        print('Ascending ', c,b,a)
+    else :
+        print('Ascending ', c,a,b)
