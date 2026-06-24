511cae1c5a2f381331513897d3becb9195521717
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-06-24 21:36:49 +0530

    Day 21 - continue statement to skip invalid iterations

---
+++ coaching/coaching/coaching/coaching/Day21_Problem80.py
@@ -0,0 +1,10 @@
+# Problem80
+for i in range(4):
+     a= int(input('Enter Numerator : '))
+     b= int(input('Enter denominator : '))
+     
+     if b==0:
+      continue
+     c=a/b
+     print(c)
+   
