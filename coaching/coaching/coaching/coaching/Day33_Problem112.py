4678e032c1560e81dfa635d11b0f4b6887b9845c
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-07-06 21:10:38 +0530

    Day 33 - nested while loop star grid pattern

---
+++ coaching/coaching/coaching/coaching/Day33_Problem112.py
@@ -0,0 +1,11 @@
+# Problem112
+a= int(input('How many rows  = '))
+b= int(input('How many columns  = '))
+i=0
+while i<a:
+    j=0
+    while j<b:
+        print('*',end=' ')
+        j=j+1
+    print()
+    i=i+1
