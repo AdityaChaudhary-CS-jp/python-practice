9e2a8d0e63cd44850a63b3573e46b112e170ce50
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-07-07 21:01:36 +0530

    Day 34 - reverse a number using while loop

--- coaching/coaching/coaching/coaching/Day34_Problem118.py
+++ coaching/coaching/coaching/coaching/Day34_Problem118.py
@@ -1,13 +1,8 @@
-# Problem117
+# Problem118
 a=int(input('Enter the no = '))
 k=0
-p=0
 while a!=0:
     r=a%10
     a=a//10
-    if r%2==0:
-        k+=1
-    else:
-        p+=1
-print('Odd = ',p)
-print('Even = ',k)
+    k=k*10+r
+print('Reverse no = ',k)
