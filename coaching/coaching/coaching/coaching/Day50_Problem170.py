38b597073ae19f82fdfd70f39f5703c4638af1f5
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-07-23 20:47:04 +0530

    Day 50 - string strip, replace, partition & split methods

---
+++ coaching/coaching/coaching/coaching/Day50_Problem170.py
@@ -0,0 +1,10 @@
+# Problem170
+a = '@@@@my@you@@@'
+print(a.strip('@'))
+print(a.lstrip('@'))
+print(a.rstrip('@'))
+print(a.replace('my','you'))
+print(a.partition('my'))
+# 
+a=input('Enter the sentence = ')
+print(a.split())
