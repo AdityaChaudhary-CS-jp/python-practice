53ff57f41f2a6362a7be6262b9ffedacdf66777d
AdityaChaudhary-CS-jp <chaudharypc70@gmail.com>
2026-07-25 21:52:28 +0530

    Day 52 - three ways to create a list

---
+++ coaching/coaching/coaching/coaching/Day52_Problem175.py
@@ -0,0 +1,14 @@
+# Problem175
+# 1
+l=list(input('Enter list = '))
+print(l)
+# 2
+l=eval(input('Enter list = '))
+print(l)
+# 3
+a=int(input('How many sets in list = '))
+p=[]
+for i in range(a):
+    k=int(input('Enter the number = '))
+    p.append(k)
+print(p)
