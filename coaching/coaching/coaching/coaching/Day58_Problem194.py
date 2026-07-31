251bd88aa2bce2e5c64b21bb64d6513d29802783
Aditya Chaudhary <chaudharypc70@gmail.com>
2026-07-31 21:54:21 +0530

    Day 58 - list methods (reverse, sort, pop, remove, insert, extend, indexing)

---
+++ coaching/coaching/coaching/coaching/Day58_Problem194.py
@@ -0,0 +1,27 @@
+# Problem194
+l=[1, 2, 3, 4, 5]
+l.reverse()
+print(l)
+b=[8,99,2,35,80,1]
+b.sort()
+print(b)
+b.sort(reverse=True)
+print(b)
+f=[2,8,9,0,4]
+print(f.pop())
+print(f.pop(-1))
+f.remove(2)
+print(f)
+l.append(6)
+print(l)
+b.insert(1,9999)
+print(b)
+c=[1,2,3]
+l.extend(c)
+print(l)
+a = [25,'seek',56,73,'great']
+print(a[1])
+print(a[1][0])
+g = '1287958'
+j = list(g)
+print(j)
