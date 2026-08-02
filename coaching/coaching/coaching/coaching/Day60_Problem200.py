1ccaa887260fb9e9c772ee0ebc795d7b94561e3c
Aditya Chaudhary <chaudharypc70@gmail.com>
2026-08-02 21:50:01 +0530

    Day 60 - list mutability vs list() copy demo

---
+++ coaching/coaching/coaching/coaching/Day60_Problem200.py
@@ -0,0 +1,15 @@
+# Problem200
+l = [7,3,4,9,11]
+b=l
+print(b)
+print(l)
+b[3]=100
+print(b)
+print(l)
+# Both list changed, if did dame for simple valuse without list they don't change
+a = [5,2,9]
+b = list(a)
+a[2] = 20
+print(a)
+print(b)
+# Use this so both list don't change
