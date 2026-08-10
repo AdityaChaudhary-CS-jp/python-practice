f413b3dbc725d5974680486e2914749373c9c142
Aditya Chaudhary <chaudharypc70@gmail.com>
2026-08-10 21:24:12 +0530

    Day 68 - function returning multiple values and unpacking tuple

---
+++ coaching/coaching/coaching/coaching/Day68_Problem220.py
@@ -0,0 +1,14 @@
+# Problem220
+def mm(k):
+    a = k+10
+    b=k-5
+    c=k+20
+    d=k-2
+    return a,b,c,d
+h=mm(int(input('Enter any number = ')))
+print(h)
+x,y,z,n = h
+print(x)
+print(y)
+print(z)
+print(n)
