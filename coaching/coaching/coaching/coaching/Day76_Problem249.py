5add3f686d5985cda39a89b90d3a38ede87e5af7
Aditya Chaudhary <chaudharypc70@gmail.com>
2026-08-18 21:45:55 +0530

    Day 76 - file pointer seek() to skip characters before reading

---
+++ coaching/coaching/coaching/coaching/Day76_Problem249.py
@@ -0,0 +1,7 @@
+# Problem249
+a = open('Problem248.py')
+a.seek(5)
+b = a.read()
+print(b)
+# with open(' ','w') as a:
+# write anything, no need to close the file, it will automatically close the file after the block is executed
