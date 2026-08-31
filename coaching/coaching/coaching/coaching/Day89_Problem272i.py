a9644b120827b4ce80af1a07424d04707542ead7
Aditya Chaudhary <283899671+AdityaChaudhary-CS-jp@users.noreply.github.com>
2026-08-31 21:22:59 +0530

    Day 89 - implementation importing Problem272 module to add unique record

---
+++ coaching/coaching/coaching/coaching/Day89_Problem272i.py
@@ -0,0 +1,8 @@
+import Problem272
+stack=[]
+name = input('Enter the name = ')
+id_ = int(input('Enter the id = '))
+mobile = int(input('Enter the mobile number = '))
+email = input('Enter the email = ')
+pin = int(input('Enter the pincode = '))
+stack.append(name)
