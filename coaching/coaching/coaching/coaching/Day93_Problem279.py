d8d600a2ab48bef8a49b58a1e6f66413a00d4baa
Aditya Chaudhary <283899671+AdityaChaudhary-CS-jp@users.noreply.github.com>
2026-09-04 18:18:18 +0530

    Day 93 - turtle graphics to draw a five-pointed star

---
+++ coaching/coaching/coaching/coaching/Day93_Problem279.py
@@ -0,0 +1,12 @@
+# Problem279
+from turtle import *
+b = Turtle()
+b.forward(300)
+b.right(144)
+b.forward(300)
+b.right(144)
+b.forward(300)
+b.right(144)
+b.forward(300)
+b.right(144)
+b.forward(300)
